520319749bb3685a95a3af2763acf4e3fdac9c15
Frank Netscher <fnetscher@tacc.utexas.edu>
2023-07-18 11:23:57 -0500

    Obscuring sensitive data in logs

--- src/data_loading.py
+++ src/data_loading.py
@@ -340,9 +340,9 @@ def get_api_subjects_json(api_root = 'https://api.a2cps.org/files/v2/download/pu
                           portal_api_root = os.environ.get('PORTAL_API_ROOT'), 
                           coresessionid = None):
     ''' Load subjects data from api. Note data needs to be cleaned, etc. to create properly formatted data product'''
-    print(coresessionid)
+    print(slice(coresessionid,5))
     tapis_token = get_tapis_token(portal_api_root, coresessionid)
-    print(tapis_token)
+    print(slice(tapis_token,5))
     auth_status = get_auth_status(vbr_api_root, tapis_token)
 
     if auth_status == True:
@@ -378,7 +378,7 @@ def get_api_subjects_json(api_root = 'https://api.a2cps.org/files/v2/download/pu
     
 def get_tapis_token(portal_api_root, coresessionid = None):
     print('get_tapis_token')
-    print(coresessionid)
+    print(slice(coresessionid,5))
     print('portal_api_root')
     print(portal_api_root)
     try:
@@ -397,7 +397,7 @@ def get_tapis_token(portal_api_root, coresessionid = None):
 def get_auth_status(vbr_api_root, tapis_token = None):
     ''' This is the function that will hit the auth check for Life Science API'''
     print('tapis_token')
-    print(tapis_token)
+    print(slice(tapis_token,5))
     print('vbr_api_root')
     print(vbr_api_root)
     try:
