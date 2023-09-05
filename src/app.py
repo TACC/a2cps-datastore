a7d31aa91dc73a7a3761814388aad416334fd1b6
Lissa Pearson <lissa.pearson@gmail.com>
2023-09-05 14:54:34 -0500

    Move local data paths to env and use github URLS instead of local files for screening and display

--- src/app.py
+++ src/app.py
@@ -7,24 +7,30 @@ import csv
 from data_loading import *
 
 # ----------------------------------------------------------------------------
+# ENV Variables
+# ----------------------------------------------------------------------------
+data_access_type = os.environ.get('DATA_ACCESS_TYPE')
+local_data_path = os.environ.get("LOCAL_DATA_PATH","")
+
+# ----------------------------------------------------------------------------
 # DATA PARAMETERS
 # ----------------------------------------------------------------------------
 current_folder = os.path.dirname(__file__)
-DATA_PATH = os.path.join(current_folder,'data')
+DATA_PATH = os.path.join(current_folder,local_data_path)
 ASSETS_PATH = os.path.join(current_folder,'assets')
 
+print(DATA_PATH)
 # ----------------------------------------------------------------------------
 # LOAD ASSETS FILES
 # ----------------------------------------------------------------------------
-asset_files_dict = {
-    'screening_sites': 'screening_sites.csv',
-    'display_terms': 'A2CPS_display_terms.csv',
-}
 
-display_terms, display_terms_dict, display_terms_dict_multi = load_display_terms(ASSETS_PATH, asset_files_dict['display_terms'])
+# Pointers to official files stored at github repository main branch
+screening_sites_github_url = 'https://raw.githubusercontent.com/TACC/a2cps-datastore-weekly/main/src/assets/screening_sites.csv'
+display_terms_github_url = 'https://raw.githubusercontent.com/TACC/a2cps-datastore-weekly/main/src/assets/A2CPS_display_terms.csv'
 
-screening_sites = pd.read_csv(os.path.join(ASSETS_PATH,asset_files_dict['screening_sites']))
-
+# load display terms and screening sites
+screening_sites = pd.read_csv(screening_sites_github_url)
+display_terms, display_terms_dict, display_terms_dict_multi = load_display_terms_from_github(display_terms_github_url)
 
 # Columns used in reports [UPDATE THIS IF START TO USE MORE]
 subjects_raw_cols_for_reports = ['ewcomments',
@@ -127,129 +133,136 @@ app.debug = True
 def api_apis():
     return jsonify(api_data_index)
 
-@app.route("/api/imaging")
-def api_imaging():
-    global datetime_format
-    global api_data_index
-    global api_data_cache
-    try:
-        if not api_data_index['imaging'] or not check_data_current(datetime.strptime(api_data_index['imaging'], datetime_format)):
-            if data_access_type != 'LOCAL':
-                data_date = datetime.now().strftime(datetime_format)
-                imaging_data = get_api_imaging_data(request)
-            else:
-                data_date = local_data_date
-                imaging_data = get_local_imaging_data(DATA_PATH)
-            
-            if imaging_data:
-                api_data_index['imaging'] = data_date
-                api_data_cache['imaging'] = imaging_data
-                
-        return jsonify({'date': api_data_index['imaging'], 'data': api_data_cache['imaging']})
-    except Exception as e:
-        traceback.print_exc()
-        return jsonify('error: {}'.format(e))
-
-@app.route("/api/consort")
-def api_consort():
-    global datetime_format
-    global api_data_index
-    global api_data_cache
-    # try:
-    if not api_data_index['consort'] or not check_data_current(datetime.strptime(api_data_index['consort'], datetime_format)):
-        api_date = datetime.now().strftime(datetime_format)
-        consort_data_json = get_api_consort_data(request)
-        if consort_data_json:
-            api_data_cache['consort'] = consort_data_json
-            api_data_index['consort'] = api_date
-    return jsonify({'date': api_data_index['consort'], 'data': api_data_cache['consort']})
-    # except Exception as e:
-    #     traceback.print_exc()
-    #     return jsonify('error: {}'.format(e))
-
-# get_api_consort_data
-@app.route("/api/blood")
-def api_blood():
-    global datetime_format
-    global api_data_index
-    global api_data_cache
-    try:
-        if not api_data_index['blood'] or not check_data_current(datetime.strptime(api_data_index['blood'], datetime_format)):
-            api_date = datetime.now().strftime(datetime_format)
-            blood_data, blood_data_request_status = get_api_blood_data(request)
-            if blood_data:
-                api_data_index['blood'] = api_date
-                api_data_cache['blood'] = blood_data
-
-            with open('requests.csv', 'a', newline='') as f:
-                writer = csv.writer(f)
-                for i in blood_data_request_status:
-                    writer.writerow(i)
-                f.close()
-
-        return jsonify({'date': api_data_index['blood'], 'data': api_data_cache['blood']})
-    except Exception as e:
-        traceback.print_exc()
-        return jsonify('error: {}'.format(e))
-
-
-@app.route("/api/subjects")
-def api_subjects():
-    global datetime_format
-    global api_data_index
-    global api_data_cache
-    global subjects_raw_cols_for_reports
-
-    try:
-        if not api_data_index['subjects'] or not check_data_current(datetime.strptime(api_data_index['subjects'], datetime_format)):
-            api_date = datetime.now().strftime(datetime_format)
-            if data_access_type != 'LOCAL':
-                data_date = datetime.now().strftime(datetime_format)
-                latest_subjects_json = get_api_subjects_json(request)
-            else:
-                data_date = local_data_date
-                latest_subjects_json = get_local_subjects_raw(DATA_PATH)
-            if latest_subjects_json:
-                # latest_data = create_clean_subjects(latest_subjects_json, screening_sites, display_terms_dict, display_terms_dict_multi)
-                latest_data = process_subjects_data(latest_subjects_json,subjects_raw_cols_for_reports,screening_sites, display_terms_dict, display_terms_dict_multi)
-
-                api_data_index['subjects'] = data_date
-                api_data_cache['subjects'] = latest_data
-                
-
-        return jsonify({'date': api_data_index['subjects'], 'data': api_data_cache['subjects']})
-    except Exception as e:
-        traceback.print_exc()
-        return jsonify('error: {}'.format(e))
+@app.route("/api/tester")
 def api_tester():
-
-    global local_subjects_data
-
-    try:
-        return jsonify(local_subjects_data)
-
-
-    except Exception as e:
-        traceback.print_exc()
-        return jsonify('error: {}'.format(e))
-
-
-@app.route("/api/full")
-def api_full():
-    datafeeds = {}
-    for data_category in api_data_cache:
-        if api_data_cache[data_category]['data']:
-                datafeeds[data_category] = list(api_data_cache[data_category]['data'].keys())
-        else:
-            datafeeds[data_category] = ['no data']
-    return jsonify(datafeeds)
-
-@app.route("/api/simple")
-def api_simple():
-    if api_data_simple['subjects']:
-        return jsonify('simple subjects')
+    if local_data_path:
+        return jsonify(local_data_path)
     else:
-        return jsonify('not found')
+        return jsonify('local_data_path not found')
+
+# @app.route("/api/imaging")
+# def api_imaging():
+#     global datetime_format
+#     global api_data_index
+#     global api_data_cache
+#     try:
+#         if not api_data_index['imaging'] or not check_data_current(datetime.strptime(api_data_index['imaging'], datetime_format)):
+#             if data_access_type != 'LOCAL':
+#                 data_date = datetime.now().strftime(datetime_format)
+#                 imaging_data = get_api_imaging_data(request)
+#             else:
+#                 data_date = local_data_date
+#                 imaging_data = get_local_imaging_data(DATA_PATH)
+            
+#             if imaging_data:
+#                 api_data_index['imaging'] = data_date
+#                 api_data_cache['imaging'] = imaging_data
+                
+#         return jsonify({'date': api_data_index['imaging'], 'data': api_data_cache['imaging']})
+#     except Exception as e:
+#         traceback.print_exc()
+#         return jsonify('error: {}'.format(e))
+
+# @app.route("/api/consort")
+# def api_consort():
+#     global datetime_format
+#     global api_data_index
+#     global api_data_cache
+#     # try:
+#     if not api_data_index['consort'] or not check_data_current(datetime.strptime(api_data_index['consort'], datetime_format)):
+#         api_date = datetime.now().strftime(datetime_format)
+#         consort_data_json = get_api_consort_data(request)
+#         if consort_data_json:
+#             api_data_cache['consort'] = consort_data_json
+#             api_data_index['consort'] = api_date
+#     return jsonify({'date': api_data_index['consort'], 'data': api_data_cache['consort']})
+#     # except Exception as e:
+#     #     traceback.print_exc()
+#     #     return jsonify('error: {}'.format(e))
+
+# # get_api_consort_data
+# @app.route("/api/blood")
+# def api_blood():
+#     global datetime_format
+#     global api_data_index
+#     global api_data_cache
+#     try:
+#         if not api_data_index['blood'] or not check_data_current(datetime.strptime(api_data_index['blood'], datetime_format)):
+#             api_date = datetime.now().strftime(datetime_format)
+#             blood_data, blood_data_request_status = get_api_blood_data(request)
+#             if blood_data:
+#                 api_data_index['blood'] = api_date
+#                 api_data_cache['blood'] = blood_data
+
+#             with open('requests.csv', 'a', newline='') as f:
+#                 writer = csv.writer(f)
+#                 for i in blood_data_request_status:
+#                     writer.writerow(i)
+#                 f.close()
+
+#         return jsonify({'date': api_data_index['blood'], 'data': api_data_cache['blood']})
+#     except Exception as e:
+#         traceback.print_exc()
+#         return jsonify('error: {}'.format(e))
+
+
+# @app.route("/api/subjects")
+# def api_subjects():
+#     global datetime_format
+#     global api_data_index
+#     global api_data_cache
+#     global subjects_raw_cols_for_reports
+
+#     try:
+#         if not api_data_index['subjects'] or not check_data_current(datetime.strptime(api_data_index['subjects'], datetime_format)):
+#             api_date = datetime.now().strftime(datetime_format)
+#             if data_access_type != 'LOCAL':
+#                 data_date = datetime.now().strftime(datetime_format)
+#                 latest_subjects_json = get_api_subjects_json(request)
+#             else:
+#                 data_date = local_data_date
+#                 latest_subjects_json = get_local_subjects_raw(DATA_PATH)
+#             if latest_subjects_json:
+#                 # latest_data = create_clean_subjects(latest_subjects_json, screening_sites, display_terms_dict, display_terms_dict_multi)
+#                 latest_data = process_subjects_data(latest_subjects_json,subjects_raw_cols_for_reports,screening_sites, display_terms_dict, display_terms_dict_multi)
+
+#                 api_data_index['subjects'] = data_date
+#                 api_data_cache['subjects'] = latest_data
+                
+
+#         return jsonify({'date': api_data_index['subjects'], 'data': api_data_cache['subjects']})
+#     except Exception as e:
+#         traceback.print_exc()
+#         return jsonify('error: {}'.format(e))
+# def api_tester():
+
+#     global local_subjects_data
+
+#     try:
+#         return jsonify(local_subjects_data)
+
+
+#     except Exception as e:
+#         traceback.print_exc()
+#         return jsonify('error: {}'.format(e))
+
+
+# @app.route("/api/full")
+# def api_full():
+#     datafeeds = {}
+#     for data_category in api_data_cache:
+#         if api_data_cache[data_category]['data']:
+#                 datafeeds[data_category] = list(api_data_cache[data_category]['data'].keys())
+#         else:
+#             datafeeds[data_category] = ['no data']
+#     return jsonify(datafeeds)
+
+# @app.route("/api/simple")
+# def api_simple():
+#     if api_data_simple['subjects']:
+#         return jsonify('simple subjects')
+#     else:
+#         return jsonify('not found')
 
 
 if __name__ == "__main__":
